959eae47ae08510a539e54bbbd0f01f519d5b12a
HyduseaMC <65610406+HyduseaMC@users.noreply.github.com>
2020-05-19 16:18:54 -0400

    Create Bee.php

---
+++ src/HyduseaMC/Entities/Bee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HyduseaMC\ULTRAMOBS\Entities;
+
+use HyduseaMC\ULTRAMOBS\Pocketmine\AddActorPacket;
+
+use pocketmine\entity\Living;
+use pocketmine\Player;
+
+class Bee extends Living
+{
+
+    public const NETWORK_ID = 122;
+
+    public $width = 0.6;
+    public $height = 0.6;
+
+    public function getName(): string
+    {
+        return "Bee";
+    }
+
+    public function getDrops(): array{
+        $lootingL = 1;
+        $cause = $this->lastDamageCause;
+        if($cause instanceof EntityDamageByEntityEvent){
+            $dmg = $cause->getDamager();
+            if($dmg instanceof Player){
+                 
+
+                $looting = $dmg->getInventory()->getItemInHand()->getEnchantment(Enchantment::LOOTING);
+                if($looting !== null){
+                    $lootingL = $looting->getLevel();
+                }else{
+                    $lootingL = 1;
+            }
+            }
+        }
+        return [Item::get(Item::Sunflower, 0, mt_rand(0, 1 * $lootingL))];
+    }
+
+}

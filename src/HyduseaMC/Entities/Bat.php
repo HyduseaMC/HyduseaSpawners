2d5fcd1363beb068af0b31bfe9fb984260d4dbc9
HyduseaMC <65610406+HyduseaMC@users.noreply.github.com>
2020-05-19 16:15:35 -0400

    Create Bat.php

---
+++ src/HyduseaMC/Entities/Bat.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HyduseaMC\ULTRAMOBS\Entities;
+
+use pocketmine\entity\Animal;
+
+class Bat extends Animal
+{
+    const NETWORK_ID = self::BAT;
+
+    public $width = 0.5;
+    public $height = 0.9;
+
+    public function getName(): string
+    {
+        return "Bat";
+    }
+    
+    
+    public function getDrops(): array{
+        $lootingL = 1;
+        $cause = $this->lastDamageCause;
+        if($cause instanceof EntityDamageByEntityEvent){
+            $dmg = $cause->getDamager();
+            if($dmg instanceof Player){
+                 
+
+                $looting = $dmg->getInventory()->getItemInHand()->getEnchantment(Enchantment::LOOTING);
+                if($looting !== null){
+                    $lootingL = $looting->getLevel();
+                }else{
+                    $lootingL = 1;
+            }
+            }
+        }
+        return [Item::get(Item::String, 0, mt_rand(0, 1 * $lootingL))];
+    }
+
+}

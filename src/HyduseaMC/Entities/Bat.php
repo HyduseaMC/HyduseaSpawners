9ab8fbb69dfa965e69b501818e190170c0c3df07
HyduseaMC <65610406+HyduseaMC@users.noreply.github.com>
2020-05-20 16:15:38 -0400

    Updated Bat :D

--- src/HyduseaMC/Entities/Bat.php
+++ src/HyduseaMC/Entities/Bat.php
@@ -15,25 +15,4 @@ class Bat extends Animal
     {
         return "Bat";
     }
-    
-    
-    public function getDrops(): array{
-        $lootingL = 1;
-        $cause = $this->lastDamageCause;
-        if($cause instanceof EntityDamageByEntityEvent){
-            $dmg = $cause->getDamager();
-            if($dmg instanceof Player){
-                 
-
-                $looting = $dmg->getInventory()->getItemInHand()->getEnchantment(Enchantment::LOOTING);
-                if($looting !== null){
-                    $lootingL = $looting->getLevel();
-                }else{
-                    $lootingL = 1;
-            }
-            }
-        }
-        return [Item::get(Item::String, 0, mt_rand(0, 1 * $lootingL))];
-    }
-
 }
